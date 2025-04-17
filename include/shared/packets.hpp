59a7b2d38c37f511c6bf5be88fdbc89a9e43b7f8
rsa1to <ryutechncode@gmail.com>
2025-04-17 11:48:21 -0700

    add action type packets

--- include/shared/packets.hpp
+++ include/shared/packets.hpp
@@ -6,86 +6,140 @@
 
 using namespace std;
 
-enum class PacketType: uint8_t {
+enum class PacketType : uint8_t
+{
     INIT,
     POSITION,
-    STRING
+    STRING,
+    ACTION
 };
 
-struct Ipacket {
+enum class ActionType : uint8_t
+{
+    FORWARD,
+    RIGHT,
+    LEFT,
+    BACK
+};
+
+struct Ipacket
+{
     virtual PacketType get_type() const = 0;
     virtual vector<char> serialize() const = 0;
     virtual ~Ipacket() = default;
 };
 
 // TESTING
-struct StringPacket: public Ipacket {
+struct StringPacket : public Ipacket
+{
     string message;
 
-    PacketType get_type() const override {
+    StringPacket(string m) : message(m) {}
+    PacketType get_type() const override
+    {
         return PacketType::STRING;
     }
-    vector<char> serialize() const override {
+    vector<char> serialize() const override
+    {
         vector<char> buffer(message.size());
         memcpy(buffer.data(), message.data(), message.size());
         return buffer;
     }
-    static StringPacket deserialize(vector<char> payload, uint16_t size) {
-        StringPacket packet;
-        packet.message = std::string(payload.begin(), payload.begin() + size);
+    static StringPacket deserialize(vector<char> payload, uint16_t size)
+    {
+        StringPacket packet(std::string(payload.begin(), payload.begin() + size));
         return packet;
     }
 };
 
-struct InitPacket : public Ipacket {
+struct InitPacket : public Ipacket
+{
     int client_id;
 
-    PacketType get_type() const override {
+    InitPacket(int id) : client_id(id) {}
+    PacketType get_type() const override
+    {
         return PacketType::INIT;
     }
-    vector<char> serialize() const override {
-        vector<char> buffer (sizeof(client_id));
+    vector<char> serialize() const override
+    {
+        vector<char> buffer(sizeof(client_id));
         memcpy(buffer.data(), &client_id, sizeof(client_id));
         return buffer;
     }
-    static InitPacket deserialize(const vector<char>& payload, uint16_t size) {
-        InitPacket packet;
-        memcpy(&packet.client_id, payload.data(), size);
+    static InitPacket deserialize(const vector<char> &payload, uint16_t size)
+    {
+        int id;
+        memcpy(&id, payload.data(), size);
+        InitPacket packet(id);
         return packet;
     }
 };
 
-struct PositionPacket : public Ipacket {
-    float x,y,z;
+struct PositionPacket : public Ipacket
+{
+    float x, y, z;
 
-    PacketType get_type() const override {
+    PositionPacket(float x, float y, float z) : x(x), y(y), z(z) {}
+    PacketType get_type() const override
+    {
         return PacketType::POSITION;
     }
-    vector<char> serialize() const override {
-        vector<char> buffer (sizeof(x) + sizeof(y) + sizeof(z));
+    vector<char> serialize() const override
+    {
+        vector<char> buffer(sizeof(x) + sizeof(y) + sizeof(z));
         memcpy(buffer.data(), &x, sizeof(x));
         memcpy(buffer.data() + sizeof(x), &y, sizeof(y));
         memcpy(buffer.data() + sizeof(x) + sizeof(y), &z, sizeof(z));
         return buffer;
     }
-    static PositionPacket deserialize(const vector<char>& payload, uint16_t size) {
-        PositionPacket packet;
-        memcpy(&packet.x, payload.data(), sizeof(float));
-        memcpy(&packet.y, payload.data() + sizeof(float), sizeof(float));
-        memcpy(&packet.z, payload.data() + 2 * sizeof(float), sizeof(float));
+    static PositionPacket deserialize(const vector<char> &payload, uint16_t size)
+    {
+        float x, y, z;
+
+        memcpy(&x, payload.data(), sizeof(float));
+        memcpy(&y, payload.data() + sizeof(float), sizeof(float));
+        memcpy(&z, payload.data() + 2 * sizeof(float), sizeof(float));
+        PositionPacket packet(x, y, z);
+
         return packet;
     }
 };
 
-inline std::unique_ptr<Ipacket> deserialize(PacketType type, vector<char> payload, uint16_t size) {
-    switch (type) {
-        case PacketType::STRING:
-            return std::make_unique<StringPacket>(StringPacket::deserialize(payload, size));
-        case PacketType::INIT:
-            return std::make_unique<InitPacket>(InitPacket::deserialize(payload, size));
-        case PacketType::POSITION:
-            return std::make_unique<PositionPacket>(PositionPacket::deserialize(payload, size));
-        default:
-            throw std::runtime_error("Unknown packet type");
+struct ActionPacket : public Ipacket
+{
+    ActionType type;
+    ActionPacket(ActionType t) : type(t) {}
+    PacketType get_type() const override
+    {
+        return PacketType::ACTION;
+    }
+    vector<char> serialize() const override
+    {
+        vector<char> buffer(sizeof(type));
+        memcpy(buffer.data(), &type, sizeof(type));
+        return buffer;
+    }
+    static ActionPacket deserialize(const vector<char> &payload, uint16_t size)
+    {
+        ActionType type;
+        memcpy(&type, payload.data(), sizeof(ActionType));
+        ActionPacket packet(static_cast<ActionType>(type));
+        return packet;
+    }
+};
+
+inline std::unique_ptr<Ipacket> deserialize(PacketType type, vector<char> payload, uint16_t size)
+{
+    switch (type)
+    {
+    case PacketType::STRING:
+        return std::make_unique<StringPacket>(StringPacket::deserialize(payload, size));
+    case PacketType::INIT:
+        return std::make_unique<InitPacket>(InitPacket::deserialize(payload, size));
+    case PacketType::POSITION:
+        return std::make_unique<PositionPacket>(PositionPacket::deserialize(payload, size));
+    default:
+        throw std::runtime_error("Unknown packet type");
     }
 }
