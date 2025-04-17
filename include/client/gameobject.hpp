fa0e06e7736cf442a0ccc414bc50caaa65f85988
rkwhitlock <reesekwhitlock@gmail.com>
2025-04-17 11:38:56 -0700

    add more functions and formatting

--- include/client/gameobject.hpp
+++ include/client/gameobject.hpp
@@ -1,8 +1,8 @@
-#include <memory>
-#include <string>
 #include "core.hpp"
 #include "cube.hpp"
 #include "transform.hpp"
+#include <memory>
+#include <string>
 
 #ifndef OBJECT_HPP
 #define OBJECT_HPP
@@ -12,27 +12,42 @@ class GameObject
 protected:
     std::string id;
     bool interactable;
-    Cube* model;
-    Transform* transform;
+
+    // change to model class
+    Cube *model;
+    Transform *transform;
     Shader shader;
 
     // displayed in view or not
     bool active;
 
 public:
-    GameObject(const std::string &objectId = "", bool interactable = false, Shader shader = Shader());
+    GameObject(const std::string &objectId = "", bool interactable = false,
+               Shader shader = Shader());
 
     virtual ~GameObject();
 
-    virtual void activate();
-    virtual void deactivate();
+    void activate();
+    void deactivate();
     bool isActive() const;
 
-    const std::string &getId() const;
-    void setId(const std::string &newId);
+    void setInteractability(bool canInteract);
+    bool isInteractable() const;
 
-    void Update(Transform* tf, float deltaTime);
-    void Render(const glm::mat4& viewProjMtx);
+    const std::string &getId() const;
+
+    // probably don't want to change id of objects
+    // void setId(const std::string &newId);
+
+    const Cube *getModel() const;
+    void setModel(const Cube *newModel);
+
+    glm::vec3 getPosition() const;
+    glm::vec3 getRotation() const;
+    glm::vec3 getScale() const;
+
+    void Update(Transform *tf, float deltaTime);
+    void Render(const glm::mat4 &viewProjMtx);
 };
 
 #endif // OBJECT_HPP
--- src/client/gameobject.cpp
+++ src/client/gameobject.cpp
@@ -1,28 +1,28 @@
 #include "client/gameobject.hpp"
 
 GameObject::GameObject(const std::string &objectId, bool canInteract, Shader sd)
-    : id(objectId), interactable(canInteract), active(true), transform(new Transform()), shader(sd), model(new Cube())
+    : id(objectId), interactable(canInteract), active(true), transform(new Transform()), shader(sd),
+      model(new Cube())
 {
 }
 
 GameObject::~GameObject() {}
 
 void GameObject::activate() { active = true; }
-
 void GameObject::deactivate() { active = false; }
-
 bool GameObject::isActive() const { return active; }
+
+bool GameObject::isInteractable() const { return interactable; }
+void GameObject::setInteractability(bool canInteract) { interactable = canInteract; }
 
 const std::string &GameObject::getId() const { return id; }
 
-void GameObject::setId(const std::string &newId) { id = newId; }
+// void GameObject::setId(const std::string &newId) { id = newId; }
 
-void GameObject::Update(Transform* tf, float deltaTime)
-{
-    model->update(tf);
-}
+glm::vec3 GameObject::getPosition() const { return transform->getPosition(); }
+glm::vec3 GameObject::getRotation() const { return transform->getRotation(); }
+glm::vec3 GameObject::getScale() const { return transform->getScale(); }
 
-void GameObject::Render(const glm::mat4& viewProjMtx)
-{
-    model->draw(viewProjMtx, shader);
-}
+void GameObject::Update(Transform *tf, float deltaTime) { model->update(tf); }
+
+void GameObject::Render(const glm::mat4 &viewProjMtx) { model->draw(viewProjMtx, shader); }
