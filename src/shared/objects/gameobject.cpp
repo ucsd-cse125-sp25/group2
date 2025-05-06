eadfeba80384bc5a4c8bfa33bddfed04a6521688
rsa1to <ryutechncode@gmail.com>
2025-05-05 23:37:32 -0700

    remove gameobject from object/

--- src/shared/objects/gameobject.cpp
+++
@@ -1,7 +0,0 @@
-#include "shared/gameobject.hpp"
-
-void GameObject::Update(Transform *tf, float deltaTime) { model->Update(tf); }
-
-void GameObject::Render(const glm::mat4 &viewProjMtx) {
-  model->Draw(viewProjMtx, shader);
-}
