a903fd5e3e4901068de63fe45ccdca5ea24ba4a7
rsa1to <ryutechncode@gmail.com>
2025-05-06 22:52:17 -0700

    remove old cube, create a new one that extends gameobject

--- include/shared/objects/cube.hpp
+++ include/shared/objects/cube.hpp
@@ -1,36 +1,11 @@
 #pragma once
 
-#include "client/shader.hpp"
-#include "shared/core.hpp"
-#include "shared/objects/cube.hpp"
-#include "shared/transform.hpp"
+#include "shared/gameobject.hpp"
 
-#include <memory>
-#include <vector>
-
-using namespace std;
-
-class Cube {
-private:
-  GLuint VAO;
-  GLuint VBO_positions, VBO_normals, EBO;
-
-  glm::mat4 model;
-  glm::vec3 color;
-
-  // Cube Information
-  vector<glm::vec3> positions;
-  vector<glm::vec3> normals;
-  vector<unsigned int> indices;
-
+class Cube : public GameObject {
 public:
-  Cube(glm::vec3 cubeMin = glm::vec3(-1, -1, -1),
-       glm::vec3 cubeMax = glm::vec3(1, 1, 1));
-  ~Cube();
-
-  void draw(const glm::mat4 &viewProjMtx, unique_ptr<Shader> &shader);
-  // void update(Transform *transform);
-  void update();
-
-  void spin(float deg);
+    Cube(const int objectId, const bool isActive, unique_ptr<Transform> &tf)
+        : GameObject(objectId, isActive, tf) {
+        type = ObjectType::CUBE;
+    }
 };
