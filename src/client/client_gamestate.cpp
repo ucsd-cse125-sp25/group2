d1610fccc0e428a31c7fa3d17601bb8889ff383c
rkwhitlock <110847014+rkwhitlock@users.noreply.github.com>
2025-05-13 20:07:47 +0000

    Formatted✨

--- src/client/client_gamestate.cpp
+++ src/client/client_gamestate.cpp
@@ -6,41 +6,34 @@
 
 using json = nlohmann::json;
 
-bool ClientGameState::init()
-{
-    ObjectLoader objectLoader = ObjectLoader();
-    objectList = objectLoader.loadObjects();
-    player = objectList.begin()->second.get();
+bool ClientGameState::init() {
+  ObjectLoader objectLoader = ObjectLoader();
+  objectList = objectLoader.loadObjects();
+  player = objectList.begin()->second.get();
 
-    return true;
+  return true;
 }
 
-void ClientGameState::update(int id, Transform *tf)
-{
-    auto obj = getObject(id);
-    if (obj)
-    {
-        obj->update(tf);
-    }
+void ClientGameState::update(int id, Transform *tf) {
+  auto obj = getObject(id);
+  if (obj) {
+    obj->update(tf);
+  }
 }
 
-void ClientGameState::draw(const glm::mat4 &viewProjMtx)
-{
-    // Draw all objects
-    for (auto it = objectList.begin(); it != objectList.end(); it++)
-    {
-        auto &object = it->second;
-        object->draw(viewProjMtx);
-    }
+void ClientGameState::draw(const glm::mat4 &viewProjMtx) {
+  // Draw all objects
+  for (auto it = objectList.begin(); it != objectList.end(); it++) {
+    auto &object = it->second;
+    object->draw(viewProjMtx);
+  }
 }
 
-GameObject *ClientGameState::getObject(int id)
-{
-    auto itr = objectList.find(id);
-    if (itr != objectList.end())
-    {
-        return itr->second.get();
-    }
-    cerr << "Object with id " << id << " not found" << endl;
-    return nullptr;
+GameObject *ClientGameState::getObject(int id) {
+  auto itr = objectList.find(id);
+  if (itr != objectList.end()) {
+    return itr->second.get();
+  }
+  cerr << "Object with id " << id << " not found" << endl;
+  return nullptr;
 }
--- src/server/server_gamestate.cpp
+++ src/server/server_gamestate.cpp
@@ -9,71 +9,65 @@
 using json = nlohmann::json;
 
 // For ServerGameState
-bool ServerGameState::init()
-{
-    ObjectLoader objectLoader = ObjectLoader();
-    objectList = objectLoader.loadObjects();
+bool ServerGameState::init() {
+  ObjectLoader objectLoader = ObjectLoader();
+  objectList = objectLoader.loadObjects();
 
-    // if (obj->getInteractionType() != InteractionType::NONE)
-    //     {
-    //         interactableObjects[objData.id] = objectList[objData.id].get;
-    //     }
+  // if (obj->getInteractionType() != InteractionType::NONE)
+  //     {
+  //         interactableObjects[objData.id] = objectList[objData.id].get;
+  //     }
 
-    return true;
+  return true;
 }
 
-GameObject *ServerGameState::getObject(int id)
-{
-    auto itr = objectList.find(id);
-    if (itr != objectList.end())
-    {
-        return itr->second.get();
+GameObject *ServerGameState::getObject(int id) {
+  auto itr = objectList.find(id);
+  if (itr != objectList.end()) {
+    return itr->second.get();
+  }
+  cerr << "Object with id " << id << " not found" << endl;
+  return nullptr;
+}
+
+void ServerGameState::updateInteraction(int id) {
+  auto obj = getObject(id);
+  if (obj) {
+    cout << "Interacting with object: " << id << endl;
+  }
+}
+vector<int> ServerGameState::getLastUpdatedObjects() {
+  auto res = move(updatedObjectIds);
+  updatedObjectIds.clear();
+  return res;
+}
+
+void ServerGameState::updateMovement(int id, MovementType type,
+                                     glm::vec3 cameraFront) {
+  auto obj = getObject(id);
+  if (obj) {
+    // Find the direction of movement based on the camera's facing direction
+    glm::vec3 flatFront =
+        glm::normalize(glm::vec3(cameraFront.x, 0.0f, cameraFront.z));
+    glm::vec3 cameraRight =
+        glm::normalize(glm::cross(flatFront, glm::vec3(0.0f, 1.0f, 0.0f)));
+    switch (type) {
+    case MovementType::FORWARD:
+      obj->applyMovement(flatFront);
+      break;
+    case MovementType::BACKWARD:
+      obj->applyMovement(-flatFront);
+      break;
+    case MovementType::LEFT:
+      obj->applyMovement(-cameraRight);
+      break;
+    case MovementType::RIGHT:
+      obj->applyMovement(cameraRight);
+      break;
+    default:
+      cerr << "Unknown movement type" << endl;
+      break;
     }
-    cerr << "Object with id " << id << " not found" << endl;
-    return nullptr;
-}
-
-void ServerGameState::updateInteraction(int id)
-{
-    auto obj = getObject(id);
-    if (obj)
-    {
-        cout << "Interacting with object: " << id << endl;
-    }
-}
-vector<int> ServerGameState::getLastUpdatedObjects()
-{
-    auto res = move(updatedObjectIds);
-    updatedObjectIds.clear();
-    return res;
-}
-
-void ServerGameState::updateMovement(int id, MovementType type, glm::vec3 cameraFront)
-{
-    auto obj = getObject(id);
-    if (obj)
-    {
-        // Find the direction of movement based on the camera's facing direction
-        glm::vec3 flatFront = glm::normalize(glm::vec3(cameraFront.x, 0.0f, cameraFront.z));
-        glm::vec3 cameraRight = glm::normalize(glm::cross(flatFront, glm::vec3(0.0f, 1.0f, 0.0f)));
-        switch (type)
-        {
-        case MovementType::FORWARD:
-            obj->applyMovement(flatFront);
-            break;
-        case MovementType::BACKWARD:
-            obj->applyMovement(-flatFront);
-            break;
-        case MovementType::LEFT:
-            obj->applyMovement(-cameraRight);
-            break;
-        case MovementType::RIGHT:
-            obj->applyMovement(cameraRight);
-            break;
-        default:
-            cerr << "Unknown movement type" << endl;
-            break;
-        }
-        updatedObjectIds.push_back(id);
-    }
+    updatedObjectIds.push_back(id);
+  }
 }
