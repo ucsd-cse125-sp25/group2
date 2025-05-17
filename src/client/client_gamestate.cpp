230d92cccd1d3f6e12580fec42eeda5666013cc8
Skyler Goh <97629350+SkylerGoh@users.noreply.github.com>
2025-05-17 03:06:27 -0700

    Update client_gamestate.cpp

--- src/client/client_gamestate.cpp
+++ src/client/client_gamestate.cpp
@@ -4,28 +4,6 @@ bool ClientGameState::init() {
   ObjectLoader objectLoader = ObjectLoader();
   objectList = objectLoader.loadObjects();
   player = objectList.begin()->second.get();
-
-  int objectId;
-  int isActive;
-  float posX, posY, posZ;
-  float rotX, rotY, rotZ;
-  float scaleX, scaleY, scaleZ;
-  string modelPath;
-  string vertShaderPath, fragShaderPath;
-
-  while (in.read_row(objectId, isActive, posX, posY, posZ, rotX, rotY, rotZ,
-                     scaleX, scaleY, scaleZ, modelPath, vertShaderPath,
-                     fragShaderPath)) {
-    auto tf = make_unique<Transform>(glm::vec3(posX, posY, posZ),
-                                     glm::vec3(rotX, rotY, rotZ),
-                                     glm::vec3(scaleX, scaleY, scaleZ));
-    auto obj = make_unique<GameObject>(objectId, isActive, tf);
-    obj->setModel(make_unique<Model>(modelPath.c_str()));
-    obj->setShader(
-        make_unique<Shader>(vertShaderPath.c_str(), fragShaderPath.c_str()));
-    objectList[objectId] = move(obj);
-    player = obj.get();
-  }
   state = Gamestate::STARTSCREEN;
   return true;
 }
