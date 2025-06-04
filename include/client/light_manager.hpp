112c2bfff800080c5d6f46bc1137a9c363ba9e63
rsa1to <ryutechncode@gmail.com>
2025-06-04 15:10:22 -0700

    light working..?

--- include/client/light_manager.hpp
+++ include/client/light_manager.hpp
@@ -19,41 +19,15 @@ struct Light {
   bool useAttenuation;
 };
 
-static unordered_map<LEVEL_ID, vector<Light>> lights;
-static LEVEL_ID currentLevel;
-
 class LightManager {
+private:
+  static unordered_map<LEVEL_ID, vector<Light>> lights;
+  static LEVEL_ID currentLevel;
 public:
-  static void setCurrentLevel(LEVEL_ID level) { currentLevel = level; }
-
   static void addLight(LEVEL_ID level, const glm::vec3 &pos,
                        const glm::vec3 &color, float radius,
-                       bool useAtten = true) {
-    if (level < NUM_LEVELS) {
-      lights[level].push_back({pos, color, radius, useAtten});
-    }
-  }
-
-  static void clearLights() { lights.clear(); }
-
-  static void sendToShader(unique_ptr<Shader> &shader) {
-    auto currentLights = lights[currentLevel];
-    int numLights = static_cast<int>(currentLights.size());
-
-    shader->setInt("numLights", numLights);
-
-    for (int i = 0; i < numLights; i++) {
-      string base = "lightPositions[" + to_string(i) + "]";
-      shader->setVec3(base, currentLights[i].position);
-
-      base = "lightColors[" + to_string(i) + "]";
-      shader->setVec3(base, currentLights[i].color);
-
-      base = "lightRadii[" + to_string(i) + "]";
-      shader->setFloat(base, currentLights[i].radius);
-
-      base = "useAttenuation[" + to_string(i) + "]";
-      shader->setBool(base, currentLights[i].useAttenuation);
-    }
-  }
+                       bool useAtten = true);
+  static void sendToShader(unique_ptr<Shader> &shader);
+  static void clearLights(LEVEL_ID level);
+  static void setCurrentLevel(LEVEL_ID level);
 };
---
+++ src/client/light_manager.cpp
@@ -0,0 +1,41 @@
+#include "light_manager.hpp"
+
+unordered_map<LEVEL_ID, vector<Light>> LightManager::lights;
+LEVEL_ID LightManager::currentLevel = 0;
+
+void LightManager::addLight(LEVEL_ID level, const glm::vec3 &pos,
+                          const glm::vec3 &color, float radius,
+                          bool useAtten) {
+  if (level < NUM_LEVELS) {
+    lights[level].push_back({pos, color, radius, useAtten});
+  }
+}
+
+void LightManager::sendToShader(unique_ptr<Shader> &shader) {
+  auto& currentLights = lights[currentLevel];
+  int numLights = currentLights.size();
+
+  shader->setInt("numLights", numLights);
+
+  for (int i = 0; i < numLights; i++) {
+    string base = "lightPositions[" + to_string(i) + "]";
+    shader->setVec3(base, currentLights[i].position);
+
+    base = "lightColors[" + to_string(i) + "]";
+    shader->setVec3(base, currentLights[i].color);
+
+    base = "lightRadii[" + to_string(i) + "]";
+    shader->setFloat(base, currentLights[i].radius);
+
+    base = "useAttenuation[" + to_string(i) + "]";
+    shader->setBool(base, currentLights[i].useAttenuation);
+  }
+}
+
+void LightManager::clearLights(LEVEL_ID level) {
+  lights[level].clear();
+}
+
+void LightManager::setCurrentLevel(LEVEL_ID level) {
+  currentLevel = level;
+}
