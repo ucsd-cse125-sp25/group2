28c2504956a3ee5f6c36ea939a584e8ecb3dee7f
rsa1to <125172564+rsa1to@users.noreply.github.com>
2025-06-05 04:42:57 +0000

    Formatted✨

--- src/server/gameserver.cpp
+++ src/server/gameserver.cpp
@@ -78,7 +78,8 @@ void GameServer::updateGameState() {
       GameStatePacket statePacket(Gamestate::GAME);
       network->sendToAll(statePacket);
       // game->getLevelManager()->advanceLevel();
-      // LevelChangePacket levelChangePacket(game->getLevelManager()->getLevel());
+      // LevelChangePacket
+      // levelChangePacket(game->getLevelManager()->getLevel());
       // network->sendToAll(levelChangePacket);
       // }
       break;
