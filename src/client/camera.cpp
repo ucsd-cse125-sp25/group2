316ed0f3d3416c8cd504b3f1f42b9b1dbc11d7c1
rsa1to <ryutechncode@gmail.com>
2025-06-05 13:53:28 -0700

    camera and mill

--- src/client/camera.cpp
+++ src/client/camera.cpp
@@ -18,12 +18,12 @@ Camera::Camera()
   worldUp = cameraUp;
 
   characterCamRadius[CHICKEN] = 3.0f;
-  characterCamRadius[PIG] = 5.5f;
+  characterCamRadius[PIG] = 4.0f;
   characterCamRadius[SHEEP] = 6.0f;
   characterCamRadius[COW] = 6.0f;
 
   characterCamOffset[CHICKEN] = glm::vec3(0.0f, 2.0f, 0.0f);
-  characterCamOffset[PIG] = glm::vec3(0.0f, 3.0f, 0.0f);
+  characterCamOffset[PIG] = glm::vec3(0.0f, 2.5f, 0.0f);
   characterCamOffset[SHEEP] = glm::vec3(0.0f, 4.3f, 0.0f);
   characterCamOffset[COW] = glm::vec3(0.0f, 4.8f, 0.0f);
 }
