56edf65d8ea8d499d3bedff804d0dd8d1fc487c9
JC Yan <jcyan@ucsd.edu>
2025-06-05 14:18:20 -0700

    puzzles, sound fix

--- src/client/sound_manager.cpp
+++ src/client/sound_manager.cpp
@@ -19,7 +19,7 @@ bool SoundManager::init() {
   if (result != FMOD_OK)
     return false;
 
-  SoundManager::setMasterVolume(0.0f);
+  SoundManager::setMasterVolume(1.0f);
   preLoadSounds();
 
   SoundManager::setVolume("BounceSound", 4.0f);
